acdc64b996db71f31c6df1bb794fcb624584e358
SHAKIL <shakilkhanking@gmail.com>
2023-08-02 22:28:25 +0600

    Update

---
+++ bin/dart_practice.dart
@@ -0,0 +1,25 @@
+void main(){
+  const int a = 20;
+  const int b = 12;
+
+  //perform_addition
+  int sum = a + b;
+  print('Addition: $sum');
+
+  //perform_subtraction
+  sum = a - b;
+  print('Subtraction: $sum');
+
+  //perform_multiplication
+  sum = a * b;
+  print('Multiplication: $sum');
+
+  //perform_division
+  double result = a / b;
+  print('Division: $result');
+
+  //perform_modulus
+  int modulusResult = a % b;
+  print('Modulus: $modulusResult');
+
+}
